5f3ad57cfcab54a595522fc6ea772d72a4c05006
Kyle Baum <username@users.noreply.github.com>
2016-11-06 15:39:15 -0600

    save 1

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,29 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square
+    @number = params["id"].to_i
+
+    @square = @number*@number
+  end
+
+  def square_root
+    @number = params["id"].to_i
+    @square_root = Math.sqrt(@number)
+
+  end
+
+  def random
+    @min = params["min"].to_i
+    @max = params["max"].to_i
+
+
+  end
+  def payment
+    @interest_rate = params["rate"]
+    @number_of_years = params["years"].to_i
+    @principal_value = params["principal"].to_i
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+
+  get("/square/:id", { :controller => "calculations", :action => "square" })
+  get("/square_root/:id", { :controller => "calculations", :action => "square_root" })
+
+  get("/random/:min/:max", { :controller => "calculations", :action => "random" })
+
+  get("/payment/:rate/:years/:principal", { :controller => "calculations", :action => "payment" })
+
 end
